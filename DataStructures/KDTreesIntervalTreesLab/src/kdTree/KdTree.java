6e819e4674e22d536d7a194d2ebe5f3572fce2c2
Kalin <k.primov92@gmail.com>
2017-09-15 03:55:28 +0300

    Added KD Tree implementation.

--- DataStructures/KDTreesIntervalTreesLab/src/kdTree/KdTree.java
+++ DataStructures/KDTreesIntervalTreesLab/src/kdTree/KdTree.java
@@ -46,11 +46,60 @@ public class KdTree {
     }
 
     public boolean contains(Point2D point) {
-        throw new UnsupportedOperationException();
+        Node node = this.contains(this.root, point, 0);
+        return node != null;
+    }
+
+    private Node contains(Node node, Point2D point, int depth) {
+        if(node == null) {
+            return null;
+        }
+        int cmp = this.compare(point, node.point2D, depth);
+        if(cmp == 0) {
+            return node;
+        }
+
+        if(cmp < 0) {
+            return this.contains(node.left, point, depth + 1);
+        } else {
+            return this.contains(node.right, point, depth + 1);
+        }
     }
 
     public void insert(Point2D point) {
-        throw new UnsupportedOperationException();
+        this.root = this.insert(this.root, point, 0);
+    }
+
+    private Node insert(Node node, Point2D point, int depth) {
+        if (node == null) {
+            return new Node(point);
+        }
+
+        int cmp = compare(point, node.getPoint2D(), depth);
+        if (cmp < 0) {
+            node.setLeft(this.insert(node.getLeft(), point, depth + 1));
+        } else if (cmp > 0) {
+            node.setRight(this.insert(node.getRight(), point, depth + 1));
+        }
+
+        return node;
+    }
+
+    private int compare(Point2D newPoint, Point2D currentPoint, int depth) {
+        int result = 0;
+        if (depth % 2 == 0) {
+            result = Double.compare(newPoint.getX(), currentPoint.getX());
+            if(result == 0) {
+                result  = Double.compare(newPoint.getY(), currentPoint.getY());
+            }
+        } else {
+            result  = Double.compare(newPoint.getY(), currentPoint.getY());
+            if(result == 0) {
+                result = Double.compare(newPoint.getX(), currentPoint.getX());
+            }
+        }
+
+        return result;
     }
 
     public void eachInOrder(Consumer<Point2D> consumer) {
